ebeb682e11b7af84f832bf27c3a303e45ddbebf0
frossy <cbarghorn@gmail.com>
2024-03-02 02:46:48 +0100

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+console.log("hello world");
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Hello, world!</h2>
+    <n-button type="primary"> testing button </n-button>
+  </div>
+</template>
